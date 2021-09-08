52f28ffefcdada5a6020b1609e2c9100b37a7100
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-09-08 18:59:53 +0200

    refactoring and dummy component

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+// const Home = require("./client/components/Home");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    // const content = renderToString(<Home/>);
+    // console.log(content);
+});
+
+app.listen(PORT, () => console.log("Listening on port: " + PORT));
---
+++ server/src/client/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default Home = () => {
+    return (
+        <div>
+            <h1>Hello World from Home</h1>
+        </div>
+    )
+}
